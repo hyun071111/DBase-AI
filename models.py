e55dc7d39c96ed401ce7c8a129868baff1367b95
hyun071111 <jisaki07@gmail.com>
2025-07-10 20:25:52 +0900

    fix: token 테이블 삭제

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
 
@@ -28,16 +29,11 @@ class UserCompany(db.Model):
     work_start_date = db.Column(db.Date)
     work_end_date = db.Column(db.Date)
 
-class Token(db.Model):
-    __tablename__ = 'token'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    refresh_token = db.Column(db.String(512), nullable=False)
-
 class Experience(db.Model):
     __tablename__ = 'experience'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    type = db.Column(db.String(50))  # project, experience, award
+    type = db.Column(db.String(50))
     date = db.Column(db.Date, nullable=True)
     name = db.Column(db.String(255))
     description = db.Column(db.Text)
@@ -65,15 +61,15 @@ class JobInformation(db.Model):
     __tablename__ = 'job_information'
     id = db.Column(db.Integer, primary_key=True)
     company_id = db.Column(db.Integer, db.ForeignKey('company_information.id'))
-    job_title = db.Column(db.String(255))
+    job_title = db.Column(db.Text)
     recruitment_count = db.Column(db.Integer)
     job_description = db.Column(db.Text)
-    qualifications = db.Column(db.String(255))
-    working_hours = db.Column(db.String(255))
-    work_type = db.Column(db.String(255))
-    required_documents = db.Column(db.String(255)) 
-    internship_pay = db.Column(db.String(50))
-    salary = db.Column(db.String(50))
+    qualifications = db.Column(db.Text)
+    working_hours = db.Column(db.Text)
+    work_type = db.Column(db.Text)
+    required_documents = db.Column(db.Text) 
+    internship_pay = db.Column(db.String(100))
+    salary = db.Column(db.String(100))
     additional_requirements = db.Column(db.Text)
 
     applications = db.relationship("ApplicationStatus", backref="job", cascade="all, delete-orphan")
@@ -84,10 +80,10 @@ class ApplicationStatus(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     job_id = db.Column(db.Integer, db.ForeignKey('job_information.id'))
     status = db.Column(db.String(50))
-    feedbac = db.Column(db.Text, nullable=True)
+    feedbac = db.Column(db.Text, nullable=True) # 'feedback'의 오타로 보이지만 일단 유지합니다.
 
 class PresentCompany(db.Model):
     __tablename__ = 'present_company'
     id = db.Column(db.Integer, primary_key=True)
     company_id = db.Column(db.Integer, db.ForeignKey('company_information.id'))
-    address = db.Column(db.Text)
+    address = association_proxy('company', 'address')
