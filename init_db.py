8e1a48bad9c18be32ef4242bcade0af7161f1221
hyun071111 <jisaki07@gmail.com>
2025-07-10 17:37:31 +0900

    fix: 데이터베이스 생성/수정 기능 수정

--- init_db.py
+++ init_db.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app
-from models import db
+from models import db, User, UserCompany, Token, Experience, CompanyInformation, JobInformation, ApplicationStatus, PresentCompany
 
 # 애플리케이션 팩토리를 사용하여 app 인스턴스 생성
 # 이렇게 하면 app의 설정(예: DB URI)을 로드할 수 있습니다.
@@ -16,13 +16,12 @@ def init_database():
         db.drop_all()
         
         print("새로운 테이블을 생성합니다.")
+        # 임포트된 모든 모델 클래스를 기반으로 테이블을 생성합니다.
         db.create_all()
         
         print("데이터베이스 초기화가 완료되었습니다.")
 
 if __name__ == '__main__':
-    # 스크립트가 직접 실행될 때만 데이터베이스 초기화 함수를 호출합니다.
-    # 사용 전 확인을 위한 간단한 프롬프트 추가
     confirm = input("경고: 이 스크립트는 데이터베이스의 모든 데이터를 삭제하고 스키마를 재생성합니다.\n계속하시겠습니까? (yes/no): ")
     if confirm.lower() == 'yes':
         init_database()
